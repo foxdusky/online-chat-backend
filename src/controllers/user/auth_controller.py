56fa917debaf9d72b79e6e5996c65c6c14035e03
fox <78961902+foxdusky@users.noreply.github.com>
2024-10-23 01:33:56 +0300

    feat: deploy script

--- src/controllers/user/auth_controller.py
+++ src/controllers/user/auth_controller.py
@@ -4,7 +4,6 @@ from sqlmodel import Session
 
 from db import get_session
 from models.user import auth_model
-from models.user.auth_model import get_current_user
 from schemes.user.user_scheme import User
 from schemes.user.auth_scheme import AuthToken
 
@@ -23,13 +22,12 @@ async def sign_in(
     return token
 
 
-@auth_router.get(
-    "/generate_token/",
-    response_model=AuthToken,
-    description="Returns JWT token by user database id and expiry in minutes")
-async def generate_token(
-        user_id: int,
-        expiry: int | None,
-        current_user: User = Depends(get_current_user)
+@auth_router.post("/registration/", response_model=AuthToken)
+async def reg(
+        user: User,
+        session: Session = Depends(get_session),
+
 ):
-    return auth_model._get_auth_token(user_id, expiry)
+    # token = auth_model.sign_in(session)
+    # return token
+    pass
